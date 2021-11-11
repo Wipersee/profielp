36825c09c7705e473d69674b3cbd97ab8541f222
Wipersee <kesterera@gmail.com>
2021-11-11 15:34:09 +0200

    feat: added normal admin pannel to orders section

--- orders/models.py
+++ orders/models.py
@@ -4,16 +4,20 @@ import uuid
 
 class OrderStatus(models.Model):
     order_statuses = [
-        ('CRTD', 'Created'),
-        ('ACCPTD', 'Accepted'),
-        ('INPRGRS', 'In progress'),
-        ('ADMN', 'Complaint under consideration by the administrator')
+        ("CRTD", "Created"),
+        ("ACCPTD", "Accepted"),
+        ("INPRGRS", "In progress"),
+        ("ADMN", "Complaint under consideration by the administrator"),
     ]
 
     # Fields
-    order_status_id = models.UUIDField(primary_key=True, default=uuid.uuid4, )
-    order_status = models.CharField(max_length=16, choices=order_statuses,
-                                    help_text="Order status")
+    order_status_id = models.UUIDField(
+        primary_key=True,
+        default=uuid.uuid4,
+    )
+    order_status = models.CharField(
+        max_length=16, choices=order_statuses, help_text="Order status"
+    )
 
     class Meta:
         verbose_name_plural = "Order statuses"
@@ -25,20 +29,32 @@ class OrderStatus(models.Model):
 
 class Complaint(models.Model):
     # Fields
-    complaint_id = models.UUIDField(primary_key=True, default=uuid.uuid4, )
+    complaint_id = models.UUIDField(
+        primary_key=True,
+        default=uuid.uuid4,
+    )
 
     admin_id = models.ForeignKey(
         "users.Admin",
         on_delete=models.DO_NOTHING,
-        help_text="ID of the Admin that assigned to the complaint"
+        help_text="ID of the Admin that assigned to the complaint",
     )
-    requester_id = models.ForeignKey("users.User", editable=False, on_delete=models.DO_NOTHING, related_name='+')
+    requester_id = models.ForeignKey(
+        "users.User", on_delete=models.DO_NOTHING, related_name="+"
+    )
 
-    comment = models.TextField(help_text="Requester complaint text", blank=True, null=True)
+    comment = models.TextField(
+        help_text="Requester complaint text", blank=True, null=True
+    )
     admin_comment = models.TextField(help_text="Admin comment", blank=True, null=True)
 
-    date = models.DateTimeField(help_text="Date and time of the complaint creating", )
-    resolve_date = models.DateTimeField(help_text="Date and time of the complaint resolving", )
+    date = models.DateTimeField(
+        help_text="Date and time of the complaint creating", auto_now_add=True
+    )
+    resolve_date = models.DateTimeField(
+        help_text="Date and time of the complaint resolving", null=True, blank=True
+    )
+    resolved = models.BooleanField(default=False)
 
     class Meta:
         verbose_name_plural = "Complaints"
@@ -50,27 +66,54 @@ class Complaint(models.Model):
 
 class Order(models.Model):
     # Fields
-    order_id = models.UUIDField(primary_key=True, default=uuid.uuid4, )
+    order_id = models.UUIDField(
+        primary_key=True,
+        default=uuid.uuid4,
+    )
 
-    customer_id = models.ForeignKey("users.Customer", on_delete=models.DO_NOTHING, related_name='+')
-    performer_id = models.ForeignKey("users.Performer", on_delete=models.DO_NOTHING, related_name='+')
+    customer_id = models.ForeignKey(
+        "users.Customer", on_delete=models.DO_NOTHING, related_name="+"
+    )
+    performer_id = models.ForeignKey(
+        "users.Performer", on_delete=models.DO_NOTHING, related_name="+"
+    )
 
-    order_status_id = models.ForeignKey("OrderStatus", on_delete=models.DO_NOTHING, related_name='+')
-    complaint_id = models.OneToOneField("Complaint", null=True, blank=True, on_delete=models.DO_NOTHING, related_name='+')
+    order_status_id = models.ForeignKey(
+        "OrderStatus", on_delete=models.DO_NOTHING, related_name="+"
+    )
+    complaint_id = models.OneToOneField(
+        "Complaint",
+        null=True,
+        blank=True,
+        on_delete=models.DO_NOTHING,
+        related_name="+",
+    )
 
     address = models.TextField(help_text="Order address", blank=True, null=True)
     latitude = models.FloatField(help_text="Order address latitude")
     longitude = models.FloatField(help_text="Order address longitude")
 
-    comment = models.TextField(help_text="Order additional comment", blank=True, null=True)
+    comment = models.TextField(
+        help_text="Order additional comment", blank=True, null=True
+    )
 
-    is_high_priority = models.BooleanField(help_text="True if order is urgent", default=False)
+    is_high_priority = models.BooleanField(
+        help_text="True if order is urgent", default=False
+    )
 
-    date = models.DateTimeField(help_text="Date and time of the order creation")
-    completion_date = models.DateTimeField(null=True, blank=True, help_text="Date and time of the order completion")
+    date = models.DateTimeField(
+        help_text="Date and time of the order creation", auto_now=True
+    )
+    completion_date = models.DateTimeField(
+        null=True, blank=True, help_text="Date and time of the order completion"
+    )
 
-    customer_approved = models.BooleanField(help_text="True if customer approved the work", default=False)
-    performer_approved = models.BooleanField(help_text="True if performer approved the work", default=False)
+    customer_approved = models.BooleanField(
+        help_text="True if customer approved the work", default=False
+    )
+    performer_approved = models.BooleanField(
+        help_text="True if performer approved the work", default=False
+    )
 
     def __str__(self):
         return f"Order {self.order_id}"
