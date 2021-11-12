5ea159564a811e07df3ea3511c131621b64bfd27
Wipersee <kesterera@gmail.com>
2021-11-12 15:54:08 +0200

    fix: rewrite token lifetime

--- profielp/settings.py
+++ profielp/settings.py
@@ -138,8 +138,8 @@ AUTH_USER_MODEL = "users.User"
 
 # JWT AUTH SETTINGS
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=1),
-    "REFRESH_TOKEN_LIFETIME": timedelta(minutes=2),
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=5),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
     "ROTATE_REFRESH_TOKENS": True,
     "BLACKLIST_AFTER_ROTATION": True,
     "UPDATE_LAST_LOGIN": False,
