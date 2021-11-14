dd85d454e0b074cf99e7721e5ef2a485de6c03ee
Wipersee <kesterera@gmail.com>
2021-11-14 15:15:56 +0200

    feat: added filters on front main page

--- reactjs/src/pages/map/components/SearchGroup.js
+++ reactjs/src/pages/map/components/SearchGroup.js
@@ -1,47 +1,52 @@
 import { useState } from "react";
-import { Card, Input, Divider, Select } from "antd";
+import { Card, Input, Divider, Select, InputNumber, Form, Button, Slider } from "antd";
 import "./../css/searchgroup.css";
+import { useSelector, useDispatch } from "react-redux";
+import { fetchData } from "../../../common/fetchData";
 
 const { Search } = Input;
 const { Option } = Select;
 
 const SearchGroup = () => {
-  const [loading, setLoging] = useState(false);
-  const onSearch = (value) => {
-    setLoging(true);
-    console.log(value);
-    setTimeout(() => {
-      setLoging(false);
-    }, 3000);
-  };
-  const children = []
-  const props = ["Plumber", "Caretaker", "Carpenter", "Cobbler", "Electrician", "Gardener", "Mechanic", "Roofer", "Tiler"];
-  for (let i = 0; i < props.length; i++){
-    children.push(<Option key={i.toString(36) + i}>{props[i]}</Option>);
+  const [form] = Form.useForm();
+  const dispatch = useDispatch()
+  const { loading } = useSelector((state) => state.performersReducer)
+  const onFinish = (values) => {
+    dispatch(
+      fetchData(`/users/performers?description=${values.description}&min_price=${values.price[0]}&max_price=${values.price[1]}`, "PERFORMERS")
+    );
   }
-
   return (
     <Card className="main-card-search">
       <h2>Profielp</h2>
-      <Search
-        placeholder="Search for master"
-        allowClear
-        enterButton="Search"
-        size="large"
-        onSearch={onSearch}
-        loading={loading}
-      />
-      <Divider />
-      <h4>Filters:</h4>
-      <Select
-      mode="multiple"
-      allowClear
-      style={{ width: '100%' }}
-      placeholder="Or select profi"
-      maxTagCount='responsive'
-    >
-      {children}
-    </Select>
+      <Form
+        form={form}
+        name="register"
+        onFinish={onFinish}
+        initialValues={{
+          prefix: "38",
+        }}
+        scrollToFirstError
+      >
+        <Form.Item
+          name="description"
+        >
+          <Input />
+        </Form.Item>
+        <Divider />
+        <Form.Item
+          name="price"
+          label="Price range"
+        >
+          <Slider range defaultValue={[1, 999]} min={0} max={1000} />
+        </Form.Item>
+
+        <Form.Item>
+          <Button type="primary" htmlType="submit" loading={loading}>
+            Find
+          </Button>
+        </Form.Item>
+      </Form>
     </Card>
   );
 };
--- reactjs/src/store/reducers/performersReducer.js
+++ reactjs/src/store/reducers/performersReducer.js
@@ -1,12 +1,20 @@
-const initialState = { performers: [] }
+let initialState = {
+    data: [],
+    loading: false,
+    hasErrors: false,
+};
 
-export const performersReducer = (state = initialState, action) => {
+const performersReducer = (state = initialState, action) => {
     switch (action.type) {
-        case 'SET_PERFORMERS':
-            return { ...state, performers: action.payload }
+        case "GET_PERFORMERS_DATA":
+            return { ...state, loading: true };
+        case "GET_PERFORMERS_DATA_SUCCESS":
+            return { data: action.payload, loading: false, hasErrors: false };
+        case "GET_PERFORMERS_DATA_FAILURE":
+            return { ...state, loading: false, hasErrors: true };
         default:
-            return state
+            return state;
     }
-}
+};
 
 export default performersReducer;
